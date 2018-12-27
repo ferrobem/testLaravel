1a8231b32bd636c3fddb99a19577424b1288e817
ferrobem <fernandolucassud@gmail.com>
2018-12-27 17:57:10 -0200

    Test Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Test Commit
                 </div>
 
                 <div class="links">
